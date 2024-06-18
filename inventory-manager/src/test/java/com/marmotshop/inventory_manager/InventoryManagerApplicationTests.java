047d5bcfdad29c64a1f2918740d26f3a0cf9aee9
Yuanke <yuankemiao.dev@gmail.com>
2024-06-18 12:40:45 +0300

    initialized spring boot

---
+++ inventory-manager/src/test/java/com/marmotshop/inventory_manager/InventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marmotshop.inventory_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
